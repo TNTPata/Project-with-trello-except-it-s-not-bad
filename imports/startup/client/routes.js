baf45db0975514d9867205be518bdf42ab28f2c2
TNTPata <putte.olsson@icloud.com>
2019-05-15 14:09:15 +0200

    Added a bunch of needed files
    Don't know what they do, but they're important. I think.

---
+++ imports/startup/client/routes.js
@@ -0,0 +1,23 @@
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
+// Import needed layouts
+import '../../ui/layouts/basic/basic.js';
+
+// Import needed templates
+import '../../ui/pages/test/test.js';
+
+const publicRoutes = FlowRouter.group({
+	name: 'public',
+	triggersEnter: [() => {
+		window.scroll(0,0);
+	}],
+});
+
+// Set up routes in app
+publicRoutes.route('/', {
+	name: 'Home',
+	action() {
+		BlazeLayout.render('Basic', {main:'Clock'});
+	},
+});
---
+++ imports/ui/pages/test/test.js
@@ -0,0 +1,38 @@
+import './test.html';
+
+import { ReactiveDict } from 'meteor/reactive-dict';
+
+Template.Clock.onCreated(function() {
+	this.state = new ReactiveDict({
+		hours: null,
+		minutes: null,
+		seconds: null,
+	});
+	
+	const self = this;
+	Meteor.setInterval(function() {
+		const time = new Date();
+		const hours = time.getHours();
+		const minutes = time.getMinutes();
+		const seconds = time.getSeconds();
+		self.state.set('hours', (hours > 9) ? hours : '0'+hours );
+		self.state.set('minutes', (minutes > 9) ? minutes : '0'+minutes );
+		self.state.set('seconds', (seconds > 9) ? seconds : '0'+seconds );
+	},100);
+});
+
+Template.Clock.helpers({
+	hours() {
+		return Template.instance().state.get('hours');
+	},
+	minutes() {
+		return Template.instance().state.get('minutes');
+	},
+	seconds() {
+		return Template.instance().state.get('seconds');
+	},
+});
+
+Template.Clock.events({
+
+});
